33fe9bde992d76cba47c1c4d0c30dbd6c51b0b55
Advait Pathak <advaitpathak2@gmail.com>
2022-12-12 15:35:17 +0530

    First Commit

---
+++ app/main/consumer/consumer_app.py
@@ -0,0 +1,88 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from user_data_processor import *
+
+
+def get_raw_df(spark, schema, alias_value):
+    """
+            :param spark: spark session for the streaming app
+            :param alias_value: The alias value for the clean data frame
+            :return: raw spark dataframe from the kafka consumer
+    """
+    """
+         Creating a consumer for the kafka prodcuer running inside a docker container 
+         exposed at localhost:9093.
+         Setting spark.streaming.receiver.maxRate
+         as could not use default timestamp value available in streams
+         as it was difficult to aggregate them.
+         Setting scheduler mode as 'FAIR' to run multiple aggregations in a fair mechanism
+    """
+    kafka_topic = "data-stream-analysis"
+    kafka_servers = '192.168.3.214:29093'
+
+    return spark \
+        .readStream \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", kafka_servers) \
+        .option("subscribe", kafka_topic) \
+        .option("startingOffsets", "earliest") \
+        .option("spark.streaming.backpressure.enabled", True) \
+        .option("spark.streaming.receiver.maxRate", "2") \
+        .option("spark.scheduler.mode", "FAIR") \
+        .load() \
+        .selectExpr("CAST(value AS STRING)") \
+        .select(from_json(col("value"), schema)) \
+        .alias(alias_value)
+        # .select("jsontostructs(value).*") \
+
+
+def get_spark_streaming_session():
+    """
+        :return: Returns a spark streaming object
+    """
+    scala_version = '2.12'
+    spark_version = '3.3.0'
+    # TODO: Ensure match above values match the correct versions
+    packages = [
+        f'org.apache.spark:spark-sql-kafka-0-10_{scala_version}:{spark_version}',
+        'org.apache.kafka:kafka-clients:3.2.1'
+    ]
+    spark = SparkSession.builder \
+        .master("local") \
+        .appName("kafka-example") \
+        .config("spark.jars.packages", ",".join(packages)) \
+        .getOrCreate()
+
+    # spark = SparkSession \
+    #     .builder \
+    #     .master("local[2]") \
+    #     .appName("Spark-Data-Processor") \
+    #     .getOrCreate()
+
+    sc = spark.sparkContext
+    sc.setLogLevel("ERROR")
+    return spark
+
+
+def process_user_data(spark, user_schema, alias_value):
+    """
+        :param user_schema: schema which needs to applied on to the incoming data stream
+        :param alias_value: The alias value for the clean data frame
+        :return: None
+    """
+    df_clean = get_raw_df(spark, user_schema, alias_value)
+    UserDataProcessor.get_gender_by_counts(df_clean).start()
+    UserDataProcessor.get_country_by_counts(df_clean).start()
+
+
+def start():
+    spark = get_spark_streaming_session()
+    process_user_data(spark, USER_SCHEMA, USER_ALIAS)
+    spark.streams.awaitAnyTermination()
+
+
+if __name__ == "__main__":
+    """
+    Entry point of the spark streaming application
+    """
+    start()
