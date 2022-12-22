7f48df8cac7f0b28f33ac963d1d6f93f2b65fdde
Advait Pathak <advaitpathak2@gmail.com>
2022-12-22 13:46:29 +0530

    config commit

--- app/main/config.py
+++ app/main/config.py
@@ -1,4 +1,6 @@
-KAFKA_SERVERS = "192.168.3.214:29093"
-# KAFKA_SERVERS = "192.168.1.3:29093"
+KAFKA_SERVERS = "<your-ip>:29093"
+
 DATA_STREAM_ANALYSIS = "data-stream-analysis"
+DATA_STREAM_OUTPUT = "data-stream-output"
+
 GITHUB_ANALYSIS = "github-analysis"
